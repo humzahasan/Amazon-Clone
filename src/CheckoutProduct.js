ecc59a2466ac1c30cd64d3588e94be10d12b895e
bravopy <humzahasan0126@gmail.com>
2020-09-11 17:04:26 +0530

    Handled some console error

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,37 +1,44 @@
-import React,{forwardRef} from 'react';
+import React, {forwardRef} from 'react';
 import './CheckoutProduct.css';
 
+import {v4 as uuidv4} from 'uuid';
 import {useStateValue} from './StateProvider';
-const CheckoutProduct = forwardRef(({id, image, title, price, rating,key},ref) => {
-  const [{basket}, dispatch] = useStateValue();
-  const removeFromBasket = () => {
-    dispatch({
-      type: 'REMOVE_FROM_BASKET',
-      id: id,
-    });
-  };
+const CheckoutProduct = forwardRef(
+  ({id, image, title, price, rating, key}, ref) => {
+    const [{basket}, dispatch] = useStateValue();
+    const removeFromBasket = () => {
+      dispatch({
+        type: 'REMOVE_FROM_BASKET',
+        id: id,
+      });
+    };
 
-  return (
-    <div ref={ref} className='checkoutProduct'>
-      <img src={image} alt='prod' className='checkoutProduct__image' />
+    return (
+      <div ref={ref} className='checkoutProduct'>
+        <img src={image} alt='prod' className='checkoutProduct__image' />
 
-      <div className='checkoutProduct__info'>
-        <p className='checkoutProduct__title'>{title}</p>
-        <p className='checkoutProduct__price'>
-          <small>₹</small>
-          <strong>{price}</strong>
-        </p>
-        <div className='checkoutProduct__rating'>
-          {Array(rating)
-            .fill()
-            .map((_, i) => (
-              <p>🌟</p>
-            ))}
+        <div className='checkoutProduct__info'>
+          <p className='checkoutProduct__title'>{title}</p>
+          <p className='checkoutProduct__price'>
+            <small>₹</small>
+            <strong>{price}</strong>
+          </p>
+          <div className='checkoutProduct__rating'>
+            {Array(rating)
+              .fill()
+              .map((_, i) => (
+                <p key={uuidv4()}>
+                  <span role='img' aria-label='star'>
+                    🌟
+                  </span>
+                </p>
+              ))}
+          </div>
+          <button onClick={removeFromBasket}>Remove from basket</button>
         </div>
-        <button onClick={removeFromBasket}>Remove from basket</button>
       </div>
-    </div>
-  );
-})
+    );
+  }
+);
 
 export default CheckoutProduct;
--- src/Product.js
+++ src/Product.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './Product.css';
 import {useStateValue} from './StateProvider';
-import {NotificationContainer, NotificationManager} from 'react-notifications';
-
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 function Product({id, title, image, price, rating}) {
-  const [{ basket }, dispatch] = useStateValue();
-  
-  const addToBasket = () => {
+  const [{basket}, dispatch] = useStateValue();
 
+  const addToBasket = () => {
     //dispatch the item into the data layer
     dispatch({
       type: 'ADD_TO_BASKET',
@@ -22,21 +19,8 @@ function Product({id, title, image, price, rating}) {
     });
   };
 
-  const createNotification = (type) => {
-    return () => {
-      switch (type) {
-        case 'info':
-          NotificationManager.info(basket.item);
-          console.log("clicked");
-          break;
-
-        default : break;
-      }
-    }
-  };
   return (
     <div className='product'>
-      
       <div className='product__info'>
         <p>{title}</p>
         <p className='product__price'>
@@ -47,14 +31,23 @@ function Product({id, title, image, price, rating}) {
           {Array(rating)
             .fill()
             .map((_, i) => (
-              <p key={uuidv4()}>🌟</p>
+              <p key={uuidv4()}>
+                <span role='img' aria-label='star'>
+                  🌟
+                </span>
+              </p>
             ))}
         </div>
       </div>
       <img src={image} alt='' />
-     
-      <button onClick={ () => {addToBasket(); createNotification('info')() }}>Add to Basket</button>
-      <NotificationContainer/>
+
+      <button
+        onClick={() => {
+          addToBasket();
+        }}
+      >
+        Add to Basket
+      </button>
     </div>
   );
 }
